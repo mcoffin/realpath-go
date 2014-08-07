7f0a005b2d576d31a94bf94356cd9fda365054d6
Matt Coffin <mcoffin13@gmail.com>
2014-08-07 17:28:39 -0600

    Add option to only strip path

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # realpath-go
 
-A minimal implementation of the `realpath` command. Originally created because OSX lacks the command, and many installation scripts depend upon it.
+A minimal implementation of the `realpath(1)` command. Originally created because OSX lacks the command, and many installation scripts depend upon it.
 
 ## Installation
 
 To install, simply run `go get github.com/mcoffin/realpath-go/realpath`
+
+## Usage
+
+`realpath [-s] filename`
+* `-s`: Only strip the path, don't evaluate symlinks
--- realpath/realpath.go
+++ realpath/realpath.go
@@ -8,17 +8,32 @@ import (
 )
 
 func main() {
+	stripOnly := flag.Bool("s", false, "only strip the path, don't resolve symlinks")
+
 	flag.Parse()
 	args := flag.Args()
+
 	if len(args) != 1 {
 		fmt.Fprintln(os.Stderr, "Usage: realpath <path>")
 		os.Exit(1)
 	}
-	newPath, err := filepath.EvalSymlinks(args[0])
+
+	newPath, err := filepath.Abs(args[0])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Bad path: %s\n", newPath)
 		os.Exit(2)
-	} else {
-		fmt.Println(newPath)
 	}
+
+	fmt.Println(*stripOnly)
+
+	if !*stripOnly {
+		fmt.Println("Resolving symlinks")
+		newPath, err = filepath.EvalSymlinks(newPath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Bad path: %s\n", newPath)
+			os.Exit(2)
+		}
+	}
+
+	fmt.Println(newPath)
 }
